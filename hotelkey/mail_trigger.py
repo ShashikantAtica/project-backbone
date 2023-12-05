c4b2bbf35d4f9715d192cce80e44f100c9bd0e79
sqtdev <hari.softqubes@aticaglobal.com>
2023-12-05 11:09:56 +0000

    Hotelkey Script Changes

--- hotelkey/mail_trigger.py
+++ hotelkey/mail_trigger.py
@@ -1,5 +1,7 @@
 import sys
+
 sys.path.append("..")
+import argparse
 import arrow
 import requests
 from selenium import webdriver
@@ -12,10 +14,55 @@ from utils.secrets.SecretManager import get_secret_from_api as get_secret_dict
 from utils.db import db_config
 
 
+def insert_into_pulldate(PROPERTY_CODE, PULLED_DATE, PMS_NAME):
+    LAST_PULL_DATE_ID = None
+    DB_PMS_NAME = "'" + PMS_NAME + "'"
+    DB_PROPERTY_CODE = "'" + PROPERTY_CODE + "'"
+    DB_PULLED_DATE = "'" + str(PULLED_DATE) + "'"
+    DB_STATUS = "'INPROGRESS'"
+    query_string = f'INSERT INTO "tbl_pullDate" ("propertyCode", "pulledDate", "status","pmsName") VALUES ({DB_PROPERTY_CODE}, {DB_PULLED_DATE}, {DB_STATUS},{DB_PMS_NAME}) RETURNING id; '
+    conn = db_config.get_db_connection()
+    try:
+        result = conn.execute(query_string)
+        LAST_PULL_DATE_ID = result.fetchone()['id']
+        print(LAST_PULL_DATE_ID)
+        conn.close()
+        print("Added successfully!!!")
+    except Exception as e:
+        conn.close()
+        error_message = str(e)
+        print(error_message)
+    return LAST_PULL_DATE_ID
+
+
+def update_into_pulldate(LAST_PULL_DATE_ID, ERROR_NOTE, IS_ERROR):
+    # Update entry into pull date table
+    print("ERROR_NOTE :: ", ERROR_NOTE)
+    DB_STATUS = ""
+    if IS_ERROR:
+        DB_STATUS = "'FAILED'"
+    else:
+        DB_STATUS = "'FINISHED'"
+    DB_ERROR_NOTE = "'" + str(ERROR_NOTE) + "'"
+    DB_UPDATED_AT = "'" + str(arrow.now()) + "'"
+    DB_LAST_PULL_DATE_ID = "'" + str(LAST_PULL_DATE_ID) + "'"
+    query_string = f'UPDATE "tbl_pullDate" SET status={DB_STATUS}, "updatedAt"={DB_UPDATED_AT}, "errorNote"={DB_ERROR_NOTE} WHERE "id"={DB_LAST_PULL_DATE_ID};'
+    conn = db_config.get_db_connection()
+    try:
+        conn.execute(query_string)
+        conn.close()
+        print("Updated successfully!!!")
+    except Exception as e:
+        conn.close()
+        error_message = str(e)
+        print(error_message)
+
+
 def Hotelkey_Pms(row):
     atica_property_code = row['atica_property_code']
     external_property_code = row['external_property_code']
     propertyCode = row['propertyCode']
+    pullDateId = row['pullDateId']
     platform = "RMS"
     try:
         print(f"Getting Secret for {atica_property_code}")
@@ -104,46 +151,84 @@ def Hotelkey_Pms(row):
             res_report = s.post("https://motel6.aboveproperty.com/rest/v1/alerts", json=res_payload, headers=headers)
             if res_report.status_code == 200:
                 print(f"{report_type} report sent successfully to {send_email}")
+                update_into_pulldate(pullDateId, ERROR_NOTE="Mail sent successfully", IS_ERROR=False)
             else:
-                print(f"{report_type} report email sent failed")
+                msg = f"{report_type} report email sent failed"
+                print(msg)
+                update_into_pulldate(pullDateId, ERROR_NOTE=msg, IS_ERROR=True)
             # Reservation Report End
     except Exception as e:
         print(e)
+        update_into_pulldate(pullDateId, ERROR_NOTE="Something went wrong", IS_ERROR=True)
 
 
 if __name__ == '__main__':
-    # Get all property using brand
-    PMS_NAME = "'Hotelkey'"
-    print("SCRIPT STARTED FOR Hotelkey")
-    conn = db_config.get_db_connection()
-    result = conn.execute(f'SELECT * FROM tbl_properties WHERE "pmsName" = {PMS_NAME};')
-    conn.close()
-    print(result)
-    print("Fetched successfully")
-    for item in result:
-        PROPERTY_ID = item['id']
-        PROPERTY_CODE = item['propertyCode']
-        EXTERNAL_PROPERTY_CODE = item['externalPropertyCode']
-        PROPERTY_SECRET = item['propertySecret']
-        PMS_NAME = item['pmsName']
-        RES_AFTER = item['resAfter']
-        RES_BEFORE = item['resBefore']
-        OCC_AFTER = item['occAfter']
-        OCC_BEFORE = item['occBefore']
-        CURRENT_DATE = arrow.now()
+    PMS_NAME = "Hotelkey"
+    print(f"[{PMS_NAME}] SCRIPT IS STARTING...")
 
-        row = {
-            'atica_property_code': '' + PMS_NAME + '_' + EXTERNAL_PROPERTY_CODE,
-            'external_property_code': EXTERNAL_PROPERTY_CODE,
-            'gcp_secret': PROPERTY_SECRET,
-            'property_type': PMS_NAME,
-            'current_date': CURRENT_DATE,
-            'res_before': CURRENT_DATE.shift(days=-RES_BEFORE),
-            'res_after': CURRENT_DATE.shift(days=+RES_AFTER),
-            'occ_before': CURRENT_DATE.shift(days=-OCC_BEFORE),
-            'occ_after': CURRENT_DATE.shift(days=+OCC_AFTER),
-            "propertyCode": PROPERTY_CODE,
-        }
-        print("row :: ", row)
-        Hotelkey_Pms(row)
-        print("SCRIPT DONE FOR Hotelkey")
+    propertycode = None
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--propertycode", type=str, required=False, help="Type in the propertycode")
+        args = parser.parse_args()
+        propertycode = args.propertycode
+        print(f"propertycode :: {propertycode}")
+    except:
+        pass
+
+    result = None
+    if propertycode is None:
+        print("All properties run")
+        conn = db_config.get_db_connection()
+        res = conn.execute(f"""SELECT * FROM tbl_properties WHERE "pmsName" = '{PMS_NAME}';""")
+        result = res.fetchall()
+        conn.close()
+        print("Fetched successfully")
+    else:
+        print(f"{propertycode} property run")
+        conn = db_config.get_db_connection()
+        res = conn.execute(f"""SELECT * FROM tbl_properties WHERE "pmsName" = '{PMS_NAME}' and "propertyCode" = '{propertycode}';""")
+        result = res.fetchall()
+        conn.close()
+        print("Fetched successfully")
+
+    if result is not None and len(result) > 0:
+        print(f"Total Properties :: {len(result)}")
+        for item in result:
+
+            PROPERTY_ID = item['id']
+            PROPERTY_CODE = item['propertyCode']
+            EXTERNAL_PROPERTY_CODE = item['externalPropertyCode']
+            PROPERTY_SECRET = item['propertySecret']
+            PMS_NAME = item['pmsName']
+            RES_AFTER = item['resAfter']
+            RES_BEFORE = item['resBefore']
+            OCC_AFTER = item['occAfter']
+            OCC_BEFORE = item['occBefore']
+            CURRENT_DATE = arrow.now()
+            PULLED_DATE = CURRENT_DATE.date()
+
+            # Add entry into pull date table
+            LAST_PULL_DATE_ID = insert_into_pulldate(PROPERTY_CODE, PULLED_DATE, PMS_NAME)
+
+            if LAST_PULL_DATE_ID is not None:
+                row = {
+                    'atica_property_code': '' + PMS_NAME + '_' + PROPERTY_CODE,
+                    'external_property_code': EXTERNAL_PROPERTY_CODE,
+                    'gcp_secret': PROPERTY_SECRET,
+                    'property_type': PMS_NAME,
+                    'current_date': CURRENT_DATE,
+                    'res_before': CURRENT_DATE.shift(days=-RES_BEFORE),
+                    'res_after': CURRENT_DATE.shift(days=RES_AFTER),
+                    'occ_before': CURRENT_DATE.shift(days=-OCC_BEFORE),
+                    'occ_after': CURRENT_DATE.shift(days=OCC_AFTER),
+                    "propertyCode": PROPERTY_CODE,
+                    "pullDateId": LAST_PULL_DATE_ID
+                }
+                print("row :: ", row)
+                Hotelkey_Pms(row)
+            else:
+                print("LAST_PULL_DATE_ID is NULL")
+    else:
+        print(f"Property not available in database!!!")
+    print(f"[{PMS_NAME}] SCRIPT STOP!!!")
